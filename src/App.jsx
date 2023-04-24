62f2ecdff58c2c28861f95c3837a0271550db464
said1854 <msaliogullari@gmail.com>
2023-04-24 22:51:34 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header';
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <Header />
+      <h1>Hello map</h1>
+    </div>
+  );
+}
+
+export default App
