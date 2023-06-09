806a0cf862747c29f88b416e0662c83a44c1f506
said1854 <msaliogullari@gmail.com>
2023-06-10 01:23:28 +0300

    country details 2

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -13,7 +13,7 @@ const CountryDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
   const countryName = params.countryName;
-  const { shownCountries } = useContext(SiteContext);
+  const { shownCountries, language } = useContext(SiteContext);
   useEffect(() => {
     const [currentCountry] = shownCountries.filter((country) => {
       return countryName == country.name.common;
@@ -27,47 +27,93 @@ const CountryDetails = () => {
   return (
     <div className="flex flex-col justify-between min-h-screen dark:bg-darker">
       <Header />
-      <div className="w-5/6 mx-auto">
-        <button className="btn w-56 h-12 px-2" onClick={() => navigate('/')}>
-          <FontAwesomeIcon className="mr-8 text-2xl" icon={faArrowLeft} />
-          {details?.languages === 'en' ? 'Back to Main' : 'Ana Sayfaya Dön'}
-        </button>
-      </div>
       {details !== initDetail ? (
-        <div className="w-5/6 h-5/6 mx-auto text-xl text-darker p-6 container flex flex-wrap bg-light dark:bg-darker dark:text-light rounded-xl dark:border">
-          <div className="flex flex-row my-4">
-            <div className="flex flex-col m-4 rounded-2xl p-2 border shadow-lg">
-              <h1 className="flex flex-row justify-start mb-4">
-                {details?.flag} | {details?.name?.official}
-              </h1>
-              <img
-                src={details?.flags.png}
-                alt={details?.flags.alt}
-                className=""
-              />
-            </div>
-            <ul className="flex flex-col m-4 rounded-2xl p-2 border shadow-lg">
-              <li>
-                <span className="">Area: {details?.area}</span>
-              </li>
-              <li>
-                <span>Capital: {details?.capital} </span>
-              </li>
-            </ul>
-            <ul className="flex flex-col">
-              <h3 className="flex flex-row justify-start m-4">Languages</h3>
-
-              {Object.keys(details?.languages).map((key, idx) => (
-                <li key={idx}>
-                  <span>
-                    {idx + 1}-{key.toLocaleUpperCase()}:{' '}
-                  </span>
-                  <span>{details?.languages[key]}</span>
-                </li>
-              ))}
-            </ul>
+        <>
+          <div className="w-5/6 mx-auto">
+            <button
+              className="btn w-56 h-12 px-2"
+              onClick={() => navigate('/')}
+            >
+              <FontAwesomeIcon className="mr-8 text-2xl" icon={faArrowLeft} />
+              {language === 'en' ? 'Back to Main' : 'Ana Sayfaya Dön'}
+            </button>
           </div>
-        </div>
+          <div className="w-5/6 mx-auto text-xl text-darker p-6 container flex flex-wrap bg-light dark:bg-darker dark:text-light rounded-xl dark:border">
+            <div className="flex flex-row my-4">
+              <div className="flex flex-col m-4 p-2 shadow-lg">
+                <h1 className="flex flex-row justify-start mb-4">
+                  {details?.flag} | {details?.name?.official}
+                </h1>
+                <img
+                  src={details?.flags.png}
+                  alt={details?.flags.alt}
+                  className=""
+                />
+              </div>
+              <p></p>
+
+              <ul className="flex flex-col m-4 rounded-2xl p-2 border shadow-lg">
+                <li>
+                  <span className="">Area: {details?.area}</span>
+                </li>
+                <li>
+                  <span>Region: {details?.region} </span>
+                </li>
+                <li>
+                  <span>Sub Region: {details?.subregion} </span>
+                </li>
+                <li>
+                  <span>Capital: {details?.capital} </span>
+                </li>
+                <li>
+                  <span>
+                    Capital lat-lng: {details?.capitalInfo.latlng[0]}
+                    {','}
+                    {details?.capitalInfo.latlng[1]}
+                  </span>
+                </li>
+              </ul>
+              <ul className="flex flex-col m-4 rounded-2xl p-2 border shadow-lg">
+                <h3 className="flex flex-row justify-start m-4">Languages</h3>
+
+                {Object.keys(details?.languages).map((key, idx) => (
+                  <li key={idx}>
+                    <span>
+                      {idx + 1}-{key.toLocaleUpperCase()}:{' '}
+                    </span>
+                    <span>{details?.languages[key]}</span>
+                  </li>
+                ))}
+              </ul>
+              <ul className="flex flex-col m-4 rounded-2xl p-2 border shadow-lg">
+                <h3 className="flex flex-row justify-start m-4">
+                  general info
+                </h3>
+
+                <li>
+                  <span>timezones: {details?.timezones}</span>
+                </li>
+                <li>
+                  <span>Start of week: {details?.startOfWeek}</span>
+                </li>
+                <li>
+                  <span>
+                    Independent: {details?.independent ? 'True' : 'False'}
+                  </span>
+                </li>
+                {Object.keys(details?.currencies).map((key, idx) => (
+                  <li key={idx}>
+                    <span>
+                      {'currency: '}
+                      {key.toLocaleUpperCase()}
+                    </span>
+                    <span>{key.name}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </>
       ) : (
         <div className="w-5/6 mx-auto p-6 container flex flex-wrap bg-light mt-8 rounded-xl dark:border">
           <p>Loading...</p>
